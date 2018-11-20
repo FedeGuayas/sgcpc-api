0e76c19f7251834496c3eb8e51286118b2749698
Hector A Alvarez Gomez <hector.alvarez@fedeguayas.com.ec>
2018-11-20 09:55:29 -0500

    Transformers

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            //
+        ];
+    }
+}
