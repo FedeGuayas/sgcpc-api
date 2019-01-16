e7cd7eef287ae46d1ae550416a30d8807aa64f34
Hector A Alvarez Gomez <hector.alvarez@fedeguayas.com.ec>
2019-01-16 10:05:18 -0500

    Create processes and months table

--- app/Process.php
+++ app/Process.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Process extends Model
 {
-    //
+    const PRESUPUESTO_COMPARTIDO='1';
+    const PRESUPUESTO_NO_COMPARTIDO='0';
 }
--- database/migrations/2019_01_15_161940_create_processes_table.php
+++ database/migrations/2019_01_15_161940_create_processes_table.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Tabla para almacenar la planificacion de cada partida presupuestaria, su distribucion por cada mes del año
+ */
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,7 +19,25 @@ class CreateProcessesTable extends Migration
     {
         Schema::create('processes', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('partida_id')->unsigned();
+            $table->integer('area_id')->unsigned(); // area que se asigno el presupuesto
+            $table->integer('area_comp')->unsigned()->nullable(); // area con que se compartio el presupuesto
+            $table->integer('month_id')->unsigned();
+            $table->string('compartido')->default(\App\Process::PRESUPUESTO_NO_COMPARTIDO);
+            $table->decimal('plan_inicial',12,2)->unsigned()->nullable()->default(0); // permanece invariable
+            $table->decimal('monto',12,2)->unsigned()->nullable()->default(0);
+            $table->decimal('comprometido',12,2)->unsigned()->default(0);
+            $table->decimal('disponible',12,2)->unsigned()->default(0);
+            $table->decimal('ejecutado',12,2)->unsigned()->default(0);
+            $table->decimal('ahorrado',12,2)->unsigned()->default(0);
+
             $table->timestamps();
+
+            $table->foreign('partida_id')->references('id')->on('partidas');
+            $table->foreign('area_id')->references('id')->on('areas');
+            $table->foreign('month_id')->references('id')->on('months');
+
+            $table->unique(['partida_id','area_id','month_id','area_comp']);
         });
     }
 
